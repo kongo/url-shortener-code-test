cc77cf0ad8650c6e471d9d15c7da30d8b0c53ee0
Dmitry Parshenko <d.parshenko@gmail.com>
2020-05-04 17:12:11 +0300

    Dispatch actions

--- spec/url_short_web_app_spec.rb
+++ spec/url_short_web_app_spec.rb
@@ -3,32 +3,55 @@ require_relative '../url_short_web_app.rb'
 describe UrlShortWebApp do
   let(:app) { UrlShortWebApp.new }
 
-  describe '#route_name' do
+  describe '#get_route_name' do
     context 'create_slug route' do
       let(:req) { double(post?: true, get?: false, path_info: '/') }
       it 'detects route correctly' do
-        expect(app.route_name(req)).to eq(:create_slug)
+        expect(app.get_route_name(req)).to eq(:create_slug)
       end
     end
 
     context 'use_slug route' do
       let(:req) { double(post?: false, get?: true, path_info: '/foo') }
       it 'detects route correctly' do
-        expect(app.route_name(req)).to eq(:use_slug)
+        expect(app.get_route_name(req)).to eq(:use_slug)
       end
     end
 
     context 'index_slugs route' do
       let(:req) { double(post?: false, get?: true, path_info: '/') }
       it 'detects route correctly' do
-        expect(app.route_name(req)).to eq(:index_slugs)
+        expect(app.get_route_name(req)).to eq(:index_slugs)
       end
     end
 
     context 'no familiar route given' do
       let(:req) { double(post?: false, get?: false, path_info: 'invalid') }
       it 'returns nil without failing' do
-        expect(app.route_name(req)).to be_nil
+        expect(app.get_route_name(req)).to be_nil
+      end
+    end
+  end
+
+  describe '#dispatch' do
+    let(:req) { double('request') }
+
+    context 'empty route given' do
+      it 'responds with 404' do
+        expect(app.dispatch(nil, req)).to eq([404, {}, []])
+      end
+    end
+
+    context 'actual route given' do
+      it 'calls corresponding action method' do
+        expect(app).to receive(:create_slug).with(req).and_return(:create_slug_result )
+        expect(app.dispatch(:create_slug, req)).to eq(:create_slug_result)
+
+        expect(app).to receive(:use_slug).with(req).and_return(:use_slug_result)
+        expect(app.dispatch(:use_slug, req)).to eq(:use_slug_result)
+
+        expect(app).to receive(:index_slugs).with(req).and_return(:index_slugs_result)
+        expect(app.dispatch(:index_slugs, req)).to eq(:index_slugs_result)
       end
     end
   end
--- url_short_web_app.rb
+++ url_short_web_app.rb
@@ -9,14 +9,37 @@ class UrlShortWebApp
 
   def call(env)
     req = Rack::Request.new(env)
-    [
-      200,
-      {},
-      [req.to_yaml]
-    ]
+    dispatch get_route_name(req), req
+
+    # [
+    #   200,
+    #   {},
+    #   [req.to_yaml]
+    # ]
   end
 
-  def route_name(req)
+  def dispatch(route_name, req)
+    return response_404 if route_name.nil?
+    send route_name, req
+  end
+
+  def create_slug(req)
+    #
+  end
+
+  def use_slug(req)
+    #
+  end
+
+  def index_slugs(req)
+    #
+  end
+
+  def response_404
+    [404, {}, []]
+  end
+
+  def get_route_name(req)
     route = ROUTES.find { |key, val| key if val.call(req) }
     route.nil? ? nil : route.first
   end
