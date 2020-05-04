4f1dfdc4c2755a24216b11d3e862e2160f56e081
Dmitry Parshenko <d.parshenko@gmail.com>
2020-05-04 18:39:03 +0300

    Remove code dup, and make action responses correspond Rack format

--- url_short_web_app.rb
+++ url_short_web_app.rb
@@ -8,6 +8,8 @@ class UrlShortWebApp
     index_slugs: ->(req) { req.get?  && req.path_info == '/' }
   }
 
+  JSON_HEADERS = {"Content-Type" => "application/json"}
+
   def initialize(repository)
     @repo = repository
   end
@@ -25,20 +27,20 @@ class UrlShortWebApp
 
   def create_slug(req, params)
     slug = @repo.add(params['url'])
-    [201, {"Content-Type" => "application/json"}, [{ short_url: slug, url: @repo.get(slug) }.to_json] ]
+    [201, JSON_HEADERS, [{ short_url: slug, url: @repo.get(slug) }.to_json] ]
   end
 
   def use_slug(req, params)
-    url = @repo.get(req.request_path)
+    url = @repo.get(req.path_info[1..])
     if url.nil?
       response_404
     else
-      [301, "Location: #{url}", [{ url: url }.to_json] ]
+      [301, { "Location" => url }, [{ url: url }.to_json] ]
     end
   end
 
   def index_slugs(req, params)
-    [200, {"Content-Type" => "application/json"}, [{ urls: @repo.all }.to_json] ]
+    [200, JSON_HEADERS, [{ urls: @repo.all }.to_json] ]
   end
 
   def response_404
